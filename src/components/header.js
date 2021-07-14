207978c4bcbdbc7c94595ba84cc383e6df66d54b
Andrew Fleming <66144906+AndrewJFleming@users.noreply.github.com>
2021-07-13 19:11:02 -0700

    Style finetuning to header mainNav

--- src/components/header.js
+++ src/components/header.js
@@ -11,7 +11,7 @@ const Header = ({ siteTitle, siteAuthor, siteLogo }) => (
   
   <header
     style={{
-      background: `#ccc`,
+      background: `#ece9e7`,
       // backgroundImage: `url(${Background})`,
     }}
   >
